b9deab6a29ba8d3c887fcca3a0fdd2bda9a226f0
refaccionesparadieselonline-cell <refaccionesparadieselonline@gmail.com>
2025-10-06 11:55:17 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // Opcionalmente puedes dejar esto si lo usabas:
+  // experimental: { appDir: true },
+  // images: { unoptimized: true },
+  // eslint: { ignoreDuringBuilds: true },
+  // typescript: { ignoreBuildErrors: true },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
