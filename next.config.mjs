07ef88b6cbeeb07c6fc2121ca120558109ca7c7b
refaccionesparadieselonline-cell <refaccionesparadieselonline@gmail.com>
2025-10-06 12:19:24 -0600

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'cdn.shopify.com' }],
-  },
-};
-export default nextConfig;
