4771544ec8071fddaa6f36a0aab2bba5fee6deb6
refaccionesparadieselonline-cell <refaccionesparadieselonline@gmail.com>
2025-10-06 15:17:58 -0600

    Delete app/tailwind.config.js

--- app/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: { colors: { brand: "#1D66A8", brandDark: "#0F2F4A", ink: "#0B0F19" } },
-  },
-  plugins: [],
-};
