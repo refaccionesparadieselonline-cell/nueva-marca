7b29bbbc251d8193a9142ede38fe6d709c205d93
refaccionesparadieselonline-cell <refaccionesparadieselonline@gmail.com>
2025-10-07 14:41:48 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
+
 export const config = { matcher: ["/"] };
 
 export function middleware(req: Request) {
-  return NextResponse.redirect(new URL("/landing", req.url));
+  return NextResponse.redirect(new URL("/tienda", req.url));
 }
